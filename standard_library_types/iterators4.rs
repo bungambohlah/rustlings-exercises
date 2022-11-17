978b273418957f5b0c91d6dfa3be0927ab960b4c
Afif Abdillah Jusuf <bungambohlah@gmail.com>
2022-11-17 09:53:02 +0700

    feat: :sparkles: add object lifetime, iterators, and test usage for rust learning purpose

--- standard_library_types/iterators4.rs
+++ standard_library_types/iterators4.rs
@@ -1,9 +1,7 @@
 // iterators4.rs
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
-pub fn factorial(num: u64) -> u64 {
+pub fn factorial(num: u128) -> u128 {
     // Complete this function to return the factorial of num
     // Do not use:
     // - return
@@ -13,6 +11,10 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    match num {
+        0 => 1,
+        1.. => (1..=num).product(),
+    }
 }
 
 #[cfg(test)]
